72cc1f882dfeeb4ce3e18f6d9849c1e80973f8b8
Nguyễn Duy Hân <85959139+hannguyen30@users.noreply.github.com>
2023-10-27 13:21:27 +0700

    Config Prettier, CSS/SASS, Router/Layout

---
+++ src/routes/index.js
@@ -0,0 +1,29 @@
+import { HeaderOnly } from '~/components/Layout';
+
+import Home from '~/pages/Home';
+import Following from '~/pages/Home';
+import Profile from '~/pages/Home';
+import Upload from '~/pages/Home';
+const publicRoutes = [
+    {
+        path: '/',
+        component: Home,
+    },
+    {
+        path: '/following',
+        component: Following,
+    },
+    {
+        path: '/profile',
+        component: Profile,
+    },
+    {
+        path: '/upload',
+        component: Upload,
+        layout: HeaderOnly,
+    },
+];
+
+const privateRoutes = [];
+
+export { publicRoutes, privateRoutes };
